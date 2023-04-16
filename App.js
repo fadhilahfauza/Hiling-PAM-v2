177d07889e29f8a0d4a89ff04ec321cb800ea128
fadhilahfauza <fauzafadhilah@gmail.com>
2023-04-17 00:56:19 +0700

    upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
+
+import Home from "./src/Home";
+import Detail from "./src/Detail";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Detail" component={Detail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Hiling-PAM-v2
+# Hiling.id
+>Fadhilah Fauza Hamda - 120140153
+Download : <a href="https://expo.dev/artifacts/eas/86ZNBFU2t5rACKVxpzqqXh.aab">APK </a>.
+
+## Halaman Pencarian:
+<img src="assets/home.png" width="200px">
+
+## Halaman Hasil Pencarian:
+<img src="assets/hasil.png" width="200px">
